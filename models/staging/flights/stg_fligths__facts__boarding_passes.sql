04c1166d7da462354922de943cbaa0139418f313
vomachit@yandex.ru <DDLbufTcqS>
2025-01-17 17:09:11 +0300

    Lesson 2 add source

---
+++ models/staging/flights/stg_fligths__facts__boarding_passes.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+
+select 
+    ticket_no,
+    flight_id,
+    boarding_no,
+    seat_no
+from
+    {{ source('demo_src', 'boarding_passes') }}
---
+++ models/staging/flights/stg_fligths__facts__tickets.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+select 
+    ticket_no,
+	book_ref,
+	passenger_id,
+	passenger_name,
+	contact_data
+from
+    {{ source('demo_src', 'tickets') }}
